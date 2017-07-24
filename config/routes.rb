ac70e97916e2bfc945c2e7b93d7252c7373d465d
meaghanlewis <meaghanlewis@Meaghans-MacBook-Air.local>
2017-07-24 16:49:42 -0700

    Signup users in UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  # resources :users, except: [new]
 end
